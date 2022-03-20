f013cd81c0cf29527f30d280d9fe66c2850dd32d
eibrahimarisoy <eibrahimarisoy@gmail.com>
2022-03-20 18:57:41 +0300

    connect postgres db

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/Picus-Security-Golang-Bootcamp/homework-3-week-4-eibrahimarisoy/book-store-service/common/db"
 	"github.com/Picus-Security-Golang-Bootcamp/homework-3-week-4-eibrahimarisoy/csv_utils"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Set environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	// connect postgres database
+	_, err := db.NewPsqlDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read CSV file
 	books, err := csv_utils.ReadCSV("data.csv")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Books:", books)
+
 }
